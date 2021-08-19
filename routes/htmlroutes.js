9193a6b0fb3e5e0cb5f4c6d723a756ee5c7dd3b9
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-08-19 11:59:14 -0400

    htmlRoutes now issue html pages

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,12 +3,12 @@ const htmlRouter = require('express').Router()
 
 //Route: get index.html
 htmlRouter.get('*', (req, res) => {
-
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 //Route: get notes.html
 htmlRouter.get('/notes', (req, res) => {
-
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
 //export our router with routes appended
