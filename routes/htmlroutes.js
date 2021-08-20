f6644e0397ee5a6d750a1fe0a93f1341b4426d1c
ElusiveEntity <AnElusiveEntity@gmail.com>
2021-08-20 15:43:34 -0400

    Debugged and fixed routes.

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,14 +1,15 @@
 //initialize router
 const htmlRouter = require('express').Router()
+const path = require("path");
 
 //Route: get index.html
-htmlRouter.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+htmlRouter.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 //Route: get notes.html
 htmlRouter.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 
 //export our router with routes appended
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const apiRoutes = require('./routes/apiroutes')
+const htmlRoutes = require('./routes/htmlroutes')
+
+//init express
 const app = express()
 
-const PORT = 3001
+//set port
+const PORT = 3002
 
+//use middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('./public/'))
 
+//use routes
+app.use('/', htmlRoutes)
+app.use('/api', apiRoutes)
+
+//start listening
 app.listen(PORT, () => {
         console.log(`Note-Taker listening at ${PORT}`)
     }
