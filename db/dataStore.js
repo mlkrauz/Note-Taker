f8a4b6e59cd2ae971f060677c328deebf737e7e6
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-09-17 11:58:02 -0400

    Added write() and addNote()

--- db/dataStore.js
+++ db/dataStore.js
@@ -17,4 +17,28 @@ class DataStore {
     read() {
         return readFileAsync('db/db.json', 'utf8')  
     }
+
+    write(allNotes) {
+        return writeFileAsync('db/db.json', JSON.stringify(allNotes))
+    }
+
+    addNote(note) {
+        const { title, text } = note
+    
+        if (!title || !text) {
+            throw new Error("Note 'title' and 'text' cannot be blank")
+        }
+    
+        // Create new note
+        const newNote = { title, text, id: uuidv1() }
+    
+        // Get existing notes
+        const existingNotes = await this.getNotes()
+
+        // Write the existing notes back to db, plus our new note
+        this.write([...existingNotes, newNote])
+            
+        // Return the new note
+        return newNote
+      }
 }
